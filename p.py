192d50c6f482108c312967718f7ed81d9a82633c
Devansh D <Mr.devanshdalal@gmail.com>
2017-04-06 00:49:45 +0530

    added many files

--- p.py
+++ p.py
@@ -2,8 +2,8 @@
 import csv,time
 import sys,math,random
 import numpy as np 
-import pyautogui
-from Tkinter import Tk
+# import pyautogui
+# from Tkinter import Tk
 import re
 
 def csv_writer(data, path):
@@ -187,6 +187,8 @@ def get_desc_of_type(tp):
 # get_desc_of_type(all_tags[-1])
 
 
+# from model import prepare_manual
+# prepare_manual(test_data,train_data)
 
 
 def mlpredict(s):
@@ -234,8 +236,55 @@ def mlpredict(s):
 			submission[i]=res_labels
 		create_submission(submission)
 
+#######################################################################################################################
+
+def mlpredict2(s):
+	from model import learn_simple
+	descriptions,test_descriptions = [],[]
+	labels = []
+	for i,x in enumerate(train_data):
+		descriptions.append(x[1])
+		y_labels = [(1 if i in x[0].split(' ') else 0) for i in all_tags]
+		y_converted = [0]*5
+		if y_labels[0]==1 or y_labels[1]==1:
+			y_converted[0]=1+y_labels[:2].index(1)
+		if y_labels[2]==1 or y_labels[3]==1:
+			y_converted[1]=1+y_labels[2:4].index(1)
+		if y_labels[4]==1 or y_labels[5]==1 or y_labels[6]==1 or y_labels[7]==1:
+			y_converted[2]=1+y_labels[4:8].index(1)
+		if y_labels[8]==1 or y_labels[9]==1 or y_labels[10]==1 :
+			y_converted[3]=1+y_labels[8:11].index(1)
+		if y_labels[11]==1:
+			y_converted[4]=1
+		labels.append(y_converted)
+
+
+	if s!=1:
+		descriptions, test_descriptions = descriptions[:int(n*s)],descriptions[int(n*s):]
+		labels, test_labels = labels[:int(n*s)],labels[int(n*s):]
+		predictions = learn_simple(np.array(descriptions),np.array(labels),np.array(test_descriptions),np.array(test_labels))
+	else:
+		for i,x in enumerate(test_data):
+			test_descriptions.append(x[0])
+		predictions = learn_simple(np.array(descriptions),np.array(labels),np.array(test_descriptions),np.array([]))
+		submission = [[] for i in test_data]
+		for i,x in enumerate(predictions):
+			res_labels = []
+			if x[0]>0:
+				res_labels.append(all_tags[x[0]-1])
+			if x[1]>0:
+				res_labels.append(all_tags[x[1]+1])
+			if x[2]>0:
+				res_labels.append(all_tags[x[2]+3])
+			if x[3]>0:
+				res_labels.append(all_tags[x[3]+7])
+			if x[4]>0:
+				res_labels.append(all_tags[11])
+			submission[i]=res_labels
+		create_submission(submission) 
+
 # print("testing logestic regression")
-mlpredict(0.9)
+mlpredict2(0.9)
 
 
 
